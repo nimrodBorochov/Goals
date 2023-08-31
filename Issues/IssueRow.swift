17df2533b0bb2fb2091f94929c5e24161f455fa6
nimrodBorochov <nimrodBorochov@gmail.com>
2023-08-31 10:26:30 +0300

    Showing, deleting, and synchronizing issues

---
+++ Issues/IssueRow.swift
@@ -0,0 +1,52 @@
+//
+//  IssueRow.swift
+//  Issues
+//
+//  Created by nimrod borochov on 30/08/2023.
+//
+
+import SwiftUI
+
+struct IssueRow: View {
+    @EnvironmentObject var dataController: DataController
+    @ObservedObject var issue: Issue
+    
+    var body: some View {
+        NavigationLink(value: issue) {
+            HStack {
+                Image(systemName: "exclamationmark.circle")
+                    .imageScale(.large)
+                    .opacity(issue.priority == 2 ? 1 : 0)
+                
+                VStack(alignment: .leading) {
+                    Text(issue.issueTitle)
+                        .font(.headline)
+                        .lineLimit(1)
+                    
+                    Text("No Tags")
+                        .foregroundColor(.secondary)
+                        .lineLimit(1)
+                }
+                
+                Spacer()
+                
+                VStack(alignment: .trailing) {
+                    Text(issue.issueCreationDate.formatted(date: .numeric, time: .omitted))
+                        .font(.subheadline)
+                    
+                    if issue.completed {
+                        Text("CLOSED")
+                            .font(.body.smallCaps())
+                    }
+                }
+                .foregroundColor(.secondary)
+            }
+        }
+    }
+}
+
+struct IssueRow_Previews: PreviewProvider {
+    static var previews: some View {
+        IssueRow(issue: .example)
+    }
+}
